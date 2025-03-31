a7ba7b7672b72bd25bbb13ecb9f7f9212c9c1fff
Jakub Ohkado <jakuboohkado@gmail.com>
2025-04-01 02:07:31 +0900

    first commit

---
+++ app/javascript/components/GameList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Game List</h2>
+    <ul>
+      <li v-for="game in games" :key="game.id">
+        {{ game.title }} - {{ game.genre }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const games = ref([])
+
+onMounted(async () => {
+  const response = await fetch('/games.json')
+  games.value = await response.json()
+})
+</script>
