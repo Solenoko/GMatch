e45888ce0dd5db075fa63acaadabd9b1101aac3a
Jakub Ohkado <jakuboohkado@gmail.com>
2025-04-15 22:37:48 +0900

    added vite.config file

---
+++ app/javascript/entrypoints/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [RubyPlugin()],
+  build: {
+    outDir: './public/assets',
+  },
+  resolve: {
+    alias: {
+      '@': '/app/javascript/entrypoints',
+    }
+  }
+})
